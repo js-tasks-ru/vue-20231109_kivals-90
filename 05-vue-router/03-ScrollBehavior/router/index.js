6e022814e3c12ce7b7919c92c7342b8c3557e06e
kivals <kivals.90@yandex.ru>
2024-02-13 18:35:10 +0300

    feat: SPA и vue-router. Task 3

--- 05-vue-router/03-ScrollBehavior/router/index.js
+++ 05-vue-router/03-ScrollBehavior/router/index.js
@@ -41,4 +41,23 @@ export const router = createRouter({
       ],
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    // Если есть hash, прокручиваем к элементу по нему
+    if (to.hash) {
+      return { el: to.hash };
+    }
+
+    // Если есть savedPosition, возвращаем его же (переход назад/вперёд)
+    if (savedPosition) {
+      return savedPosition;
+    }
+
+    // Если оба маршрута в мета свойствах имеют saveScrollPosition - не меняем положение
+    if (to.meta.saveScrollPosition && from.meta.saveScrollPosition) {
+      return false;
+    }
+
+    // По умолчанию возвращаемся в начало
+    return { left: 0, top: 0 };
+  },
 });
