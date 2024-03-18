870af89f9d5bc8bc5a3efa6041871ec4eed50927
kivals <kivals.90@yandex.ru>
2024-03-18 12:39:43 +0300

    feat: Wrapper components. Task 6

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,9 @@
 <template>
-  <UiInput />
+  <UiInput :model-value="modelProxy" @input="handleInput" :type="type">
+    <template v-for="slot in Object.keys($slots)" #[slot]>
+      <slot :name="slot" />
+    </template>
+  </UiInput>
 </template>
 
 <script>
@@ -7,7 +11,48 @@ import UiInput from './UiInput.vue';
 
 export default {
   name: 'UiInputDate',
-
   components: { UiInput },
+  emits: ['update:modelValue'],
+  props: {
+    type: {
+      type: String,
+      default: 'date'
+    },
+    modelValue: {
+      type: Number,
+      default: null
+    },
+    step: {
+      type: Number,
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    modelProxy: {
+      get() {
+        return this.modelValue ? this.localDatetime : null;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      },
+    },
+    localDatetime() {
+      const dateISO = new Date(this.modelValue).toISOString();
+      if (this.type === 'date') {
+        return dateISO.slice(0, 10);
+      }
+      if (this.type === 'time') {
+        return dateISO.slice(11, 16);
+      }
+      return dateISO.slice(0, 16);
+    },
+  },
+  methods: {
+    handleInput(event) {
+      this.$emit('update:modelValue', event.target.valueAsNumber);
+    },
+  },
 };
 </script>
